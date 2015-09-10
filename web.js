61845debf0dc4b7d026f56e6e88602f2454af288
Cristina Swearingen <criss.foltea@yahoo.com>
2015-09-09 23:44:09 -0600

    added headershot

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 var express = require('express');
 var app = express();
-var transporter = nodemailer.createTransport(transport);
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
